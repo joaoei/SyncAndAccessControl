1fa9aafe3683bfdbaac468a3b0e9bb4a055a7f03
João Victor <victorbarboza@outlook.com>
2018-05-30 17:47:38 -0700

    Q2

--- problem2.cpp
+++ problem2.cpp
@@ -1,11 +1,16 @@
+/**
+ * Compilar: g++ problem2.cpp -o problem2 -Wall -lpthread
+ * Executar: ./problem2 
+ */
+
 #include <iostream> // cout
 #include <forward_list> // forward_list
 #include <thread> // thread
 #include <mutex> // mutex
 #include <algorithm> // find
 
-std::forward_list<int> f_list; // Lista encadeada simples.
-std::mutex mtx; // Bloqueio
+std::forward_list<int> f_list; // Lista encadeada simples compartilhada pelas threads
+std::mutex mtx; // Bloqueio compartilhado entre as threads
 
 /**
  * Método usado apenas para visualização da lista durante remoções e inserções.
@@ -30,7 +35,7 @@ void I(int value) {
 	    before_end++;
 	}
 
-	std::cout << "+++ Inserindo valueo " << value << " ao fim da lista.\n\n";
+	std::cout << "+++ Inserindo valor " << value << " ao fim da lista.\n\n";
 	f_list.insert_after(before_end, value);
 
 	print_list(f_list);
@@ -60,7 +65,7 @@ void B(int value) {
 void R(int value) {
 	mtx.lock();
 	
-	std::cout << "--- Removendo valueo " << value << " (se existir) da lista.\n\n";
+	std::cout << "--- Removendo valor " << value << " (se existir) da lista.\n\n";
 	f_list.remove(value);
 
 	print_list(f_list);
@@ -72,26 +77,26 @@ void R(int value) {
  * Método principal utilizado para criar threads e rodá-las.
  */
 int main( int argc, char * argv[] ) {
-	  std::thread threads[30]; // Alocando 30 threads
+	  std::thread threads[30]; // Vetor de 30 threads que terá 10 de cada tipo.
 
-  	int j = 0;
+  	int j = 0; // Variável auxiliar utilizada somente pra inserir/remover/buscar valores de 0 a 9 na lista.
 
-  	// Criação de 10 threads para inserir valueos de 0 a 9 na lista.
-  	for (int i=0; i<10; ++i) {
+  	// Criação de 10 threads para inserir valores de 0 a 9 na lista através da varivável auxiliar j.
+  	for (int i = 0; i < 10; ++i) {
     	threads[i] = std::thread(I, j);
     	j++;
   	}
 
-  	// Criação de 10 threads para remover valueos de 0 a 9 na lista.
+  	// Criação de 10 threads para remover valores de 0 a 9 na lista através da varivável auxiliar j.
   	j = 0;
-    for (int i=10; i<20; ++i) {
+    for (int i = 10; i < 20; ++i) {
     	threads[i] = std::thread(R, j);
     	j++;
     }
 
-    // Criação de 10 threads para buscar valueos de 0 a 9 na lista.
+    // Criação de 10 threads para buscar valores de 0 a 9 na lista através da varivável auxiliar j.
     j = 0;
-    for (int i=20; i<30; ++i) {
+    for (int i = 20; i < 30; ++i) {
     	threads[i] = std::thread(B, j);
     	j++;
     }
