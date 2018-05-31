ab94cadd07a7b5d630f0e6b20173fbd0f4373cb0
João Victor <victorbarboza@outlook.com>
2018-05-30 17:51:03 -0700

    Correções Q2

--- problem2.cpp
+++ problem2.cpp
@@ -26,13 +26,13 @@ void print_list(std::forward_list<int> &f_list) {
  * Método para inserir valores no final da lista.
  */
 void I(int value) {
-  	mtx.lock();
+	mtx.lock();
 
 	auto before_end = f_list.before_begin();
 
 	std::cout << ">>> Navegando ao fim da lista encadeada.\n";
 	for(auto it = f_list.begin(); it != f_list.end(); ++it) {
-	    before_end++;
+		before_end++;
 	}
 
 	std::cout << "+++ Inserindo valor " << value << " ao fim da lista.\n\n";
@@ -79,30 +79,32 @@ void R(int value) {
 int main( int argc, char * argv[] ) {
 	  std::thread threads[30]; // Vetor de 30 threads que terá 10 de cada tipo.
 
-  	int j = 0; // Variável auxiliar utilizada somente pra inserir/remover/buscar valores de 0 a 9 na lista.
+	int j = 0; // Variável auxiliar utilizada somente pra inserir/remover/buscar valores de 0 a 9 na lista.
 
-  	// Criação de 10 threads para inserir valores de 0 a 9 na lista através da varivável auxiliar j.
-  	for (int i = 0; i < 10; ++i) {
-    	threads[i] = std::thread(I, j);
-    	j++;
-  	}
+	// Criação de 10 threads para inserir valores de 0 a 9 na lista através da varivável auxiliar j.
+	for (int i = 0; i < 10; ++i) {
+		threads[i] = std::thread(I, j);
+		j++;
+	}
 
-  	// Criação de 10 threads para remover valores de 0 a 9 na lista através da varivável auxiliar j.
-  	j = 0;
-    for (int i = 10; i < 20; ++i) {
-    	threads[i] = std::thread(R, j);
-    	j++;
-    }
+	// Criação de 10 threads para remover valores de 0 a 9 na lista através da varivável auxiliar j.
+	j = 0;
+	for (int i = 10; i < 20; ++i) {
+		threads[i] = std::thread(R, j);
+		j++;
+	}
 
-    // Criação de 10 threads para buscar valores de 0 a 9 na lista através da varivável auxiliar j.
-    j = 0;
-    for (int i = 20; i < 30; ++i) {
-    	threads[i] = std::thread(B, j);
-    	j++;
-    }
+	// Criação de 10 threads para buscar valores de 0 a 9 na lista através da varivável auxiliar j.
+	j = 0;
+	for (int i = 20; i < 30; ++i) {
+		threads[i] = std::thread(B, j);
+		j++;
+	}
 
 	// Aguardando finalização das threads.
-	for (auto& th : threads) th.join();
+	for (auto& th : threads) {
+		th.join();
+	}
 
 	return 0;
 }
